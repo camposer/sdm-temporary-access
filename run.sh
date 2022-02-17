d6b65b2686ffd6c693b982a911134d4cb2d3d655
Rodolfo Campos <camposer@gmail.com>
2022-02-17 12:56:59 +0100

    Include call to python script

--- run.sh
+++ run.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# awk 'match($0, /b[^e]*/) {print substr($0, RSTART+1, RLENGTH-1)}
-
 git diff --name-only main | while read line; do 
-    raw_resource_name=$line
-    raw_user_email=$(bash -c "git log -p -- $line | grep Author | head -1")
-
-    resource_name=$(echo $raw_resource_name | awk '/^resources/ { gsub("resources/", "", $0); print $0 }')
-    user_email=$(echo $raw_user_email | awk 'match($0, /<[^e]>/) {print substr($0, RSTART+1, RLENGTH-1)}')
-
-    echo $resource_name
-    echo $user_email
+    resource_name=$(echo $line | awk '/^resources/ { gsub("resources/", "", $0); print $0 }')
+    if [ "$resource_name" != "" ]; then
+        raw_user_email=$(git log -p -- $line | grep Author | head -1)
+        user_email=$(echo $raw_user_email | awk 'match($0, /<.*>/) { print substr($0, RSTART+1, RLENGTH-2) }')
+        echo "About to grant temporary access to $user_email on $resource_name"
+        python3 main.py $resource_name $user_email
+    fi
 done
